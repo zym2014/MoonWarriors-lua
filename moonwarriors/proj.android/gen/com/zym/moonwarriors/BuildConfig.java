662d8aa61f0b36cee3ec7df57650c10757f115d0
zym2014 <zym2014@gmail.com>
2014-12-09 03:07:58 +0800

    Revert "上传工程代码和安装豹"
    This reverts commit 5a7943476acf0d71eeda337c5882c4458fdef6d8.

--- moonwarriors/proj.android/gen/com/zym/moonwarriors/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.zym.moonwarriors;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- moonwarriors/run-win.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-E:\Demo\cocos2d-x\quick-cocos2d-x-2.2.5\player\win\player.exe -workdir E:\Demo\cocos2d-x\quick-cocos2d-x-2.2.5\projects\moonwarriors -file scripts\main.lua -size 320x480 
